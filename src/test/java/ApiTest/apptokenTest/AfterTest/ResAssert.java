0f02e1dfd7c31806f3138fe310463eab1ee9bbef
lijing <18001192887@163.com>
2018-09-07 14:52:35 +0800

    update

--- src/test/java/ApiTest/apptokenTest/AfterTest/ResAssert.java
+++ src/test/java/ApiTest/apptokenTest/AfterTest/ResAssert.java
@@ -1,20 +1,53 @@
 package ApiTest.apptokenTest.AfterTest;
 
+import JsonHandle.JsonHandle;
+import com.alibaba.fastjson.JSONObject;
+import org.testng.Assert;
+
 /**
  * Created by lijing on 2018/6/6.
- * 结果校验：这儿值校验了返回的结果码
+ * 结果校验：这儿只校验了返回的结果码
  * 还可以扩充：返回的每个参数和相应数据库表的字段值比对
  */
 public class ResAssert {
 
-    public static boolean Result(String resultcode){
-        if(resultcode.equals("200")){
-            System.out.print("case pass");
-            return true;
-        }else{
-            System.out.print("case fail");
-            return false;
+    public static boolean ExpectResult(JSONObject resJson,String Expectkey,String Expectvalue){
+        boolean Result=false;
+        String result=resJson.getString("result"); //获取返回结果码
+        System.out.println("resultcode is "+ result);
+        if(result.equals("200")) {
+            //遍历并打印所有的返回结果数据
+            JsonHandle.getJson(resJson);
+            if (JsonHandle.checkIsExitTheValue(resJson, Expectkey, Expectvalue)) {
+                    System.out.print("case pass,得到正确的返回值");
+                    Result = true;
+            } else {
+                    System.out.print("参数返回值错误");
+                    Result = false;
+            }
+
+        } else {
+                System.out.print("结果码返回错误。"+ "result is " + result);
+                Result = false;
         }
+
+        return Result;
     }
+
+    public static boolean Result(String resultcode){
+        boolean Result=false;
+        if(resultcode.equals("200")){
+            System.out.print("结果码返回正确");
+            Result=true;
+        }else {
+            System.out.print("结果码返回错误");
+            Result=false;
+        }
+        return Result;
+    }
+
+
+
+
 
 }
